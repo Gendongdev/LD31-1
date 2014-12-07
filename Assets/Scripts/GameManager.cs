f397480b0bed52a983a9b59632d01252fddbe619
JhamesBrown <jamestron@gmail.com>
2014-12-07 13:36:33 +1300

    core gameplay working

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -7,21 +7,32 @@ using System.Collections.Generic;
 public class GameManager : MonoBehaviour 
 {
     public GameObject Player { get; set; }
+    public GameObject ThePrizePrefab { get; set; }
     public GameObject PlayerPrefab { get; set; }
     public CheckPoint CurrentCheckPoint = new CheckPoint();
     public GUIStyle MainGUIStyle;
 
+    public List<Vector3> PrizePositions = new List<Vector3> ();
+
+    public bool GameComplete { get; set; }
+
+
+    public static GameManager GM;
+
     #region Awake,Start and Update
     void Awake ()
     {
-        ThePrize.Instance.PrizeGot += PrizeGot;
+        fillPrizePositionList();
+        //ThePrize.Instance.PrizeGot += PrizeGot;
         CurrentCheckPoint.nextCheckPoint();
         Player = GameObject.FindGameObjectWithTag("Player");
+        GameComplete = false;
     }
 
     void Start()
     {
         PlayerPrefab = (GameObject)Resources.Load("Player");
+        ThePrizePrefab = (GameObject)Resources.Load("ThePrize");
     }
 
     void Update()
@@ -31,20 +42,39 @@ public class GameManager : MonoBehaviour
             Instantiate(PlayerPrefab, CurrentCheckPoint.PlayerPosition, Quaternion.identity);
             Player = GameObject.FindGameObjectWithTag("Player");
         }
+
+        
             
     }
     #endregion
 
-    void PrizeGot()
+    public void PrizeGot()
     {
+
         CurrentCheckPoint.nextCheckPoint();
+        if ((CurrentCheckPoint.CheckPointNum-1) < PrizePositions.Count)
+        {
+            CurrentCheckPoint.PrizePosition = PrizePositions[CurrentCheckPoint.CheckPointNum - 1];
+            Instantiate(ThePrizePrefab, CurrentCheckPoint.PrizePosition, Quaternion.identity);
+        }
+        else
+            GameComplete = true;
+              
     }
 
     void OnGUI ()
     {
-        GUI.BeginGroup(new Rect(10, 10, 200, 60));
-        GUI.Label(new Rect(0, 00, 200, 20), "CP" + CurrentCheckPoint.CheckPointNum, MainGUIStyle);
-        GUI.Label(new Rect(0, 20, 200, 20), "PPos" + CurrentCheckPoint.PlayerPosition, MainGUIStyle);
+        GUI.BeginGroup(new Rect(10, 10, 200, 80));
+        GUI.Label(new Rect(0, 00, 200, 20), "CP " + CurrentCheckPoint.CheckPointNum, MainGUIStyle);
+        GUI.Label(new Rect(0, 20, 200, 20), "PPos " + CurrentCheckPoint.PlayerPosition, MainGUIStyle);
+        GUI.Label(new Rect(0, 40, 200, 20), "GameComplete " + GameComplete, MainGUIStyle);
         GUI.EndGroup();
+    }
+
+    void fillPrizePositionList()
+    {
+        PrizePositions.Add(new Vector3(4.0f, 0.0f, -2.2f));
+        PrizePositions.Add(new Vector3(-4.5f, 0.0f, 5.7f));
+        PrizePositions.Add(new Vector3(4.0f, 0.0f, -2.2f));
     }
 }
--- Assets/Scripts/ThePrize.cs
+++ Assets/Scripts/ThePrize.cs
@@ -3,30 +3,15 @@ using System.Collections;
 
 public class ThePrize : WorldObject  {
 
-    public delegate void PrizeGotEvent();
-    public event PrizeGotEvent PrizeGot; 
-
     public GameManager _GameManager;
-
-    //singleton
-    private static ThePrize instance;
-
-
-    public static ThePrize Instance
-    {
-        get
-        {
-            if (instance == null)
-                instance = GameObject.FindObjectOfType(typeof(ThePrize)) as ThePrize;
-
-            return instance;
-        }
-    }
 
 	void Start () 
     {
         _GameManager = GameObject.Find("Managers").GetComponent<GameManager>();
-	
+        //instance = GameObject.FindGameObjectWithTag("ThePrize").GetComponent<ThePrize>();
+        
+
+        //instance = GameObject.FindObjectOfType(typeof(ThePrize)) as ThePrize;	
 	}
 	
 	
@@ -39,7 +24,7 @@ public class ThePrize : WorldObject  {
     {
         if (col.gameObject.tag == "Player")
         {
-            PrizeGot();              
+            _GameManager.PrizeGot();              
             Destroy(gameObject);
         }       
        
